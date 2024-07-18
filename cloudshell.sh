d7e255ffdeaba196bed6995a197aa1370659df2e
finnuser <81372071+finnuser@users.noreply.github.com>
2024-07-18 10:26:58 +0200

    Update cloudshell.sh

--- cloudshell.sh
+++ cloudshell.sh
@@ -11,3 +11,10 @@ sudo apt install --assume-yes task-xfce-desktop
 curl -L -o google-chrome-stable_current_amd64.deb \
 https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes ./google-chrome-stable_current_amd64.deb
+pulseaudio --check
+pulseaudio -D
+pactl load-module module-null-sink sink_name=virtspk sink_properties=device.description=Virtual_Speaker
+pactl load-module module-null-sink sink_name=virtmic sink_properties=device.description=Virtual_Microphone_Sink
+pactl load-module module-remap-source master=virtmic.monitor source_name=virtmic source_properties=device.description=Virtual_Microphone
+pactl load-module module-loopback latency_msec=1 source="Virtual_Speaker.monitor"
+google-chrome-stable --disable-dev-shm-usage
